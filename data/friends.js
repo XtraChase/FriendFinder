719f7972daafdf2fc91bc751f1c7c033763c5931
Chase Laptop <james.chase.richards@gmail.com>
2019-11-17 18:08:07 -0500

    added styling and script

---
+++ data/friends.js
@@ -0,0 +1,10 @@
+var friends = [
+  {
+    name: "Jack",
+    photo:
+      "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/51661642_10213105560850037_8153202019327279104_o.jpg?_nc_cat=105&_nc_oc=AQm5QfOfTl95Gn-tc2_xv4ux7sfrFZCebw0sYKRtSKJWPpqnbRDAudLPAdu3MhQtEpM&_nc_ht=scontent.fatl1-1.fna&oh=6393c5ac6a790d10a1af66b1bf5472d1&oe=5E55541D",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,111 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var path = require("path");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or
+// request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+//         ****** Main APP ******
+// Capture the form inpute
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  //     ***** Modal popup after clicking submit *****
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  // Form validation
+  function validateForm() {
+    var isValid = true;
+    $(".form-control").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+
+    $(".chosen-select").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
+
+  // If all required fields are filled
+  if (validateForm()) {
+    // Create an object for the user"s data
+    var userData = {
+      name: $("#name").val(),
+      photo: $("#photo").val(),
+      scores: [
+        $("#q1").val(),
+        $("#q2").val(),
+        $("#q3").val(),
+        $("#q4").val(),
+        $("#q5").val(),
+        $("#q6").val(),
+        $("#q7").val(),
+        $("#q8").val(),
+        $("#q9").val(),
+        $("#q10").val()
+      ]
+    };
+
+    // AJAX post the data to the friends API.
+    $.post("/api/friends", userData, function(data) {
+      // Grab the result from the AJAX post so that the best match's name and photo are displayed.
+      $("#match-name").text(data.name);
+      $("#match-img").attr("src", data.photo);
+    });
+  } else {
+    alert("Please fill out all fields before submitting!");
+  }
+});
