8b418b0b435f6b7854a52e5249c5814fbc4080e6
Chase Laptop <james.chase.richards@gmail.com>
2019-11-18 14:28:41 -0500

    server now works, new app file added

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // ==============================================================================
 
 var express = require("express");
-var path = require("path");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -38,74 +37,4 @@ require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
-});
-
-//         ****** Main APP ******
-// Capture the form inpute
-$("#submit").on("click", function(event) {
-  event.preventDefault();
-
-  //     ***** Modal popup after clicking submit *****
-  // Get the modal
-  var modal = document.getElementById("myModal");
-  modal.style.display = "block";
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  };
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-
-  // Form validation
-  function validateForm() {
-    var isValid = true;
-    $(".form-control").each(function() {
-      if ($(this).val() === "") {
-        isValid = false;
-      }
-    });
-
-    $(".chosen-select").each(function() {
-      if ($(this).val() === "") {
-        isValid = false;
-      }
-    });
-    return isValid;
-  }
-
-  // If all required fields are filled
-  if (validateForm()) {
-    // Create an object for the user"s data
-    var userData = {
-      name: $("#name").val(),
-      photo: $("#photo").val(),
-      scores: [
-        $("#q1").val(),
-        $("#q2").val(),
-        $("#q3").val(),
-        $("#q4").val(),
-        $("#q5").val(),
-        $("#q6").val(),
-        $("#q7").val(),
-        $("#q8").val(),
-        $("#q9").val(),
-        $("#q10").val()
-      ]
-    };
-
-    // AJAX post the data to the friends API.
-    $.post("/api/friends", userData, function(data) {
-      // Grab the result from the AJAX post so that the best match's name and photo are displayed.
-      $("#match-name").text(data.name);
-      $("#match-img").attr("src", data.photo);
-    });
-  } else {
-    alert("Please fill out all fields before submitting!");
-  }
 });
